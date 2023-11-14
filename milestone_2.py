eae0862da760e9eca0bb723fe2776b07e730624b
sashaTribe <sasha555.tribe@gmail.com>
2023-11-14 19:28:44 +0000

    made variable names more meaningful

--- milestone_2.py
+++ milestone_2.py
@@ -3,11 +3,11 @@ import random
 
 word_list = ['blackberry','apple','cherry','mango','orange']
 #print(word_list)
-word = random.choice(word_list)
+hangman_word = random.choice(word_list)
 #print(word)
 
-guess = input("Type in a single letter: ")
-if len(guess) == 1 and guess.isalpha():
+user_guess = input("Type in a single letter: ")
+if len(user_guess) == 1 and user_guess.isalpha():
     print("Good guess!")
 else:
     print("Oops, invalid input")
