22d9ca8c29b6d29cf4b4ace58c99f1b59bdbf3e0
sashaTribe <sasha555.tribe@gmail.com>
2023-11-14 19:17:50 +0000

    created variable that randomly choose word

--- milestone_2.py
+++ milestone_2.py
@@ -1,2 +1,9 @@
+import random
+
+
 word_list = ['blackberry','apple','cherry','mango','orange']
-print(word_list)
+#print(word_list)
+
+for i in range(3):
+    word = random.choice(word_list)
+    print(word)
