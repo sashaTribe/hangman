82eb7638e4211d5b3d2b3112c892f99f8a6ef161
sashaTribe <sasha555.tribe@gmail.com>
2023-11-14 19:22:52 +0000

    check the validity of the input

--- milestone_2.py
+++ milestone_2.py
@@ -3,7 +3,11 @@ import random
 
 word_list = ['blackberry','apple','cherry','mango','orange']
 #print(word_list)
+word = random.choice(word_list)
+#print(word)
 
-for i in range(3):
-    word = random.choice(word_list)
-    print(word)
+guess = input("Type in a single letter: ")
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops, invalid input")
